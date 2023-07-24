a301166a8886270a6bf7e2e0288ac8eda6ece27e
yoontaeho <pln0302@yonsei.ac.kr>
2023-07-24 20:47:40 +0900

    005_XAI

--- neural_networks/005_XAI/my_xai.py
+++ neural_networks/005_XAI/my_xai.py
@@ -1,15 +1,11 @@
-# ==========================================#
-# Title:  CAM on Cats vs Dogs classification
-# Author: Bogyeong Suh
-# Original code on: https://junstar92.tistory.com/152
-# Date:   2023-02-07
-# ==========================================#
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
-import cv2
+from sklearn.metrics import roc_auc_score
+
+label_text_list = {0: "Rock", 1: "Paper", 2: "Scissors"}
 
 tfds.disable_progress_bar()
 
@@ -55,7 +51,7 @@ base_model = tf.keras.applications.VGG16(
 output = tf.keras.layers.GlobalAveragePooling2D()(base_model.output)
 
 # output has two neurons for the 2 classes(dogs and cats)
-output = tf.keras.layers.Dense(2, activation="softmax")(output)
+output = tf.keras.layers.Dense(3, activation="softmax")(output)
 
 # set the inputs and outputs of the model
 model = tf.keras.Model(base_model.input, output)
@@ -70,7 +66,7 @@ model.compile(
 )
 model.summary()
 
-model.fit(train_batches, epochs=5, validation_data=validation_batches)
+model.fit(train_batches, epochs=3, validation_data=validation_batches)
 
 cam_model = tf.keras.Model(
     model.input, outputs=(model.layers[-3].output, model.layers[-1].output)
@@ -78,21 +74,13 @@ cam_model = tf.keras.Model(
 cam_model.summary()
 gap_weights = model.layers[-1].get_weights()[0]
 
-# preprocessing for test data
-augmented_test_data = test_examples.map(preprocessing)
-test_batches = augmented_test_data.batch(1)
 
 # below code is for showing the heatmap of 5 example data
+y_true = []
+y_scores = []
 for img, label in test_batches.take(5):
-    print(f"ground truth: {'dog' if label else 'cat'}")
-    """
-    Displays the class activation map of an image
- 
-    Args:
-        img (tensor) -- preprocessed input image with size 224 x 224
-        features (array) -- features of the image, shape (1, 7, 7, 512)
-        results (array) -- output of the sigmoid layer
-    """
+    print(f"ground truth: {label_text_list[label.numpy()[0]]}")
+
     features, results = cam_model.predict(img)
     features_for_img = features[0]
     prediction = results[0]
@@ -106,8 +94,20 @@ for img, label in test_batches.take(5):
 
     # visualize the results
     print(f"sigmoid output: {results}")
-    print(f"prediction: {'dog' if tf.argmax(results[0]) else 'cat'}")
+    print(f"prediction: {label_text_list[tf.argmax(results[0]).numpy()]}")
     plt.figure(figsize=(8, 8))
     plt.imshow(cam_output, cmap="jet", alpha=0.5)
     plt.imshow(tf.squeeze(img), alpha=0.5)
     plt.show()
+
+    y_true.append(label)
+    y_scores.append(results[0])
+
+y_true = np.array(y_true)
+y_scores = np.array(y_scores).reshape(-1, num_classes)
+
+# print(y_true)
+# print(y_scores)
+
+auc = roc_auc_score(y_true, y_scores, multi_class="ovr")
+print(f"AUC: {auc}")
